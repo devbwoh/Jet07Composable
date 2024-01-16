4d3296e0e236546c6fb94d7943c21c14256e2761
devbwoh <bwoh@outlook.kr>
2024-01-17 05:12:00 +0900

    💄 카운터 두 개

--- app/src/main/java/kr/ac/kumoh/ce/prof01/jet07composable/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/prof01/jet07composable/MainActivity.kt
@@ -20,8 +20,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -49,6 +47,7 @@ fun MainScreen() {
             color = MaterialTheme.colorScheme.background
         ) {
             Column {
+                Counter()
                 Counter()
             }
         }
